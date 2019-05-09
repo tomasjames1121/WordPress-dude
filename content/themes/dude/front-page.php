45f19b397ba40ce07edb7f5ce7d24ebbcb002ed1
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-09 18:32:57 +0300

    fp structure plan

--- content/themes/dude/front-page.php
+++ content/themes/dude/front-page.php
@@ -10,38 +10,23 @@
  * @package dude
  */
 
-// Featured image.
-$featured_image = '';
-if ( has_post_thumbnail() ) :
-	$featured_image = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
-else :
-	$featured_image = get_theme_file_uri( 'images/default.jpg' );
-endif;
-
 get_header();
 ?>
 
 <div id="content" class="content-area">
   <main role="main" id="main" class="site-main">
 
-    <div class="entry-header-demo">
-      <h1><span class="accent"><?php echo esc_html_e( 'dude ', 'dude' ); echo esc_attr( AIR_LIGHT_VERSION, 'dude' ); ?></span><?php echo esc_html_e( 'a WordPress starter theme', 'dude' ); ?></h1>
-    </div>
+    <!-- hard coded hero -->
 
-    <div class="block">
-      <div class="container">
+    <!-- dynamic services -->
 
-        <?php if ( have_posts() ) {
-        	while ( have_posts() ) {
-	      		the_post();
-	      		the_content();
-					}
-        } else {
-        	get_template_part( 'template-parts/content', 'none' );
-        }  ?>
+    <!-- dynamic references -->
 
-      </div>
-    </div>
+    <!-- partially dynamic cta -->
+
+    <!-- dynamic blog -->
+
+    <!-- dynamic process cta -->
 
   </main><!-- #main -->
 </div><!-- #primary -->
