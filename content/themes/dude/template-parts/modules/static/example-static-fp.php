97a26218d0172b28699b26d43a95495b1bdd6031
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-10 16:21:35 +0300

    example of static module

---
+++ content/themes/dude/template-parts/modules/static/example-static-fp.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
+ * @Date:               2019-05-10 16:20:13
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2019-05-10 16:20:57
+ *
+ * @package dude2019
+ */
+
+?>
+
+<section class="block block-example-static block-example-static-frontpage">
+  <div class="container">
+
+    <div class="content">
+
+      <h2>Moro nääs!</h2>
+      <p>en tiiä....</p>
+
+    </div>
+
+  </div>
+</section>
---
+++ content/themes/dude/template-parts/modules/static/example-static.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
+ * @Date:               2019-05-10 16:20:13
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2019-05-10 16:21:25
+ *
+ * @package dude2019
+ */
+
+?>
+
+<section class="block block-example-static">
+  <div class="container">
+
+    <div class="content">
+
+      <h2>Moro nääs!</h2>
+      <p>määäää tiiän...</p>
+
+    </div>
+
+  </div>
+</section>
