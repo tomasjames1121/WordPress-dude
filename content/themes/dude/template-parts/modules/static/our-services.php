11fba788e5f7cd4c2a05a8b07d0dec318922e74f
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-11 17:29:18 +0300

    Our services block with image and cta-links

--- content/themes/dude/template-parts/modules/static/our-services.php
+++ content/themes/dude/template-parts/modules/static/our-services.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:49:22
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-11 16:15:18
+ * @Last Modified time: 2019-05-11 17:19:11
  *
  * @package dude2019
  */
@@ -13,8 +13,10 @@
 <section class="block block-our-services">
   <div class="container">
 
-    <p class="block-title-pre" aria-describedby="block-title-our-services">Mitä me teemme?</p>
-    <h2 class="block-title" id="block-title-our-services">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+    <header class="block-head">
+      <p class="block-title-pre" aria-describedby="block-title-our-services">Mitä me teemme?</p>
+      <h2 class="block-title" id="block-title-our-services">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+    </header>
 
     <div class="cols">
       <div class="col">
