63880c7c9430feca6bd709939976fcfdbf232eeb
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-11 13:48:43 +0300

    Front page hero

--- content/themes/dude/template-parts/hero-fp.php
+++ content/themes/dude/template-parts/hero-fp.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:14:20
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-11 13:25:42
+ * @Last Modified time: 2019-05-11 13:46:46
  *
  * @package dude2019
  */
@@ -16,7 +16,7 @@
     <div class="content content-select-service">
 
       <ul>
-        <li><a href="#">Haluan uudet verkkosivut</a></li>
+        <li><a href="#" class="animate animate-underline-idle">Haluan uudet verkkosivut</a></li>
         <li><a href="#">Tarvitsen visuaalista suunnittelua</a></li>
         <li><a href="#">Kunhan vaan kahtelen...</a></li>
       </ul>
