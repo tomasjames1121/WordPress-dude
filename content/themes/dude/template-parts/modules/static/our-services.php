1b550bf3caf145eb43ac673512219295316a2f7f
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-11 17:32:39 +0300

    Col description

--- content/themes/dude/template-parts/modules/static/our-services.php
+++ content/themes/dude/template-parts/modules/static/our-services.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:49:22
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-11 17:19:11
+ * @Last Modified time: 2019-05-11 17:30:05
  *
  * @package dude2019
  */
@@ -21,14 +21,14 @@
     <div class="cols">
       <div class="col">
         <h3>Verkkosivut ja -palvelut</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ex consequat, facilisis.</p>
+        <p class="col-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ex consequat, facilisis.</p>
 
         <p class="cta-link"><a href="#">Tutustu</a></p>
       </div>
 
       <div class="col">
         <h3>Visuaalinen suunnittelu</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ex consequat, facilisis.</p>
+        <p class="col-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ex consequat, facilisis.</p>
 
         <p class="cta-link"><a href="#">Tutustu</a></p>
       </div>
