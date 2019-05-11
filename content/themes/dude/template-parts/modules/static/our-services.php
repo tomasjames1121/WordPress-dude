63aa506ae0d95b205c9091ffff9aa89ae2f6b7c7
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-11 15:59:12 +0300

    Services structure

--- content/themes/dude/template-parts/modules/static/our-services.php
+++ content/themes/dude/template-parts/modules/static/our-services.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:49:22
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-11 15:03:47
+ * @Last Modified time: 2019-05-11 15:54:45
  *
  * @package dude2019
  */
@@ -15,6 +15,22 @@
 
     <p class="block-title-pre" aria-describedby="block-title-our-services">Mitä me teemme?</p>
     <h2 class="block-title" id="block-title-our-services">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+
+    <div class="cols">
+      <div class="col">
+        <h3>Verkkosivut ja -palvelut</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ex consequat, facilisis.</p>
+
+        <p class="cta-link"><a href="#">Tutustu</a></p>
+      </div>
+
+      <div class="col">
+        <h3>Visuaalinen suunnittelu</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ex consequat, facilisis.</p>
+
+        <p class="cta-link"><a href="#">Tutustu</a></p>
+      </div>
+    </div>
 
   </div>
 </section>
