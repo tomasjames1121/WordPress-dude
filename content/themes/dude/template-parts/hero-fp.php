6a0f856d85421f0b8cc683dbbb3d38795c933288
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-11 13:25:50 +0300

    Change selection markup to list

--- content/themes/dude/template-parts/hero-fp.php
+++ content/themes/dude/template-parts/hero-fp.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:14:20
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-11 12:58:07
+ * @Last Modified time: 2019-05-11 13:25:42
  *
  * @package dude2019
  */
@@ -13,11 +13,13 @@
 <section class="block block-hero block-hero-frontpage">
   <div class="container">
 
-    <div class="content">
+    <div class="content content-select-service">
 
-      <h2><a href="#">Haluan uudet verkkosivut</a></h2>
-      <h2><a href="#">Tarvitsen visuaalista suunnittelua</a></h2>
-      <h2><a href="#">Kunhan vaan kahtelen...</a></h2>
+      <ul>
+        <li><a href="#">Haluan uudet verkkosivut</a></li>
+        <li><a href="#">Tarvitsen visuaalista suunnittelua</a></li>
+        <li><a href="#">Kunhan vaan kahtelen...</a></li>
+      </ul>
 
     </div>
 
