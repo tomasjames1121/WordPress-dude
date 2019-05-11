45a5206c86d6ae2d657c4a6a9e9c021f6b8d5a44
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-11 17:11:50 +0300

    Front hero cta link

--- content/themes/dude/template-parts/hero-fp.php
+++ content/themes/dude/template-parts/hero-fp.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:14:20
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-11 14:44:34
+ * @Last Modified time: 2019-05-11 17:09:27
  *
  * @package dude2019
  */
@@ -21,8 +21,9 @@
           <li><a href="#">Tarvitsen visuaalista suunnittelua</a></li>
           <li><a href="#">Kunhan vaan kahtelen...</a></li>
         </ul>
-      </div>
 
+        <p class="cta-link"><a href="#">Ota yhteyttä</a></p>
+      </div>
     </div>
 
     <div class="featured-image featured-image-side" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/placeholder.png');"></div>
