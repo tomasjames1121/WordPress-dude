00a4f55539566b7f696b1f8328a5f6f77f50d6a8
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-18 16:58:49 +0300

    module: text-image, remove example module

---
+++ content/themes/dude/template-parts/modules/text-image.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
+ * @Date:               2019-05-18 16:50:02
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2019-05-18 16:52:50
+ *
+ * @package dude2019
+ */
+
+$content = get_sub_field( 'content' );
+$image = get_sub_field( 'image' );
+
+if ( empty( $content ) || empty( $image ) ) {
+  return;
+} ?>
+
+<section class="block block-text-image">
+  <div class="container">
+
+    <div class="content">
+      <?php echo wpautop( $content ) ?>
+    </div>
+
+    <div class="image" style="background-image:url('<?php echo wp_get_attachment_url( $image ) ?>')"></div>
+
+  </div>
+</section>
