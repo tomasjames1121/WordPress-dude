7097614641d6ee205ea00c7a4321a0a660b125c6
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-18 16:13:05 +0300

    module: process

---
+++ content/themes/dude/template-parts/modules/static/process.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
+ * @Date:               2019-05-18 16:10:53
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2019-05-18 16:12:54
+ *
+ * @package dude2019
+ */
+?>
+
+<section class="block block-process">
+  <div class="container">
+
+    <h2>Hommahan toimii, mitäs tää käytännössä menee?</h2>
+    <h2>Älä osta sikaa säkissä sit amet, consectetur adipiscing elit.</h2>
+    <p><a href="#">Lue lisää “prosessista”</a></p>
+
+  </div>
+</section>
