594a73e92385485d464f4980766d9a4bacacda62
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-18 17:43:58 +0300

    process cta class fix

--- content/themes/dude/template-parts/modules/static/cta-process.php
+++ content/themes/dude/template-parts/modules/static/cta-process.php
@@ -3,13 +3,13 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-18 16:10:53
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2019-05-18 16:12:54
+ * @Last Modified time: 2019-05-18 17:43:33
  *
  * @package dude2019
  */
 ?>
 
-<section class="block block-process">
+<section class="block block-cta-process">
   <div class="container">
 
     <h2>Hommahan toimii, mitäs tää käytännössä menee?</h2>
