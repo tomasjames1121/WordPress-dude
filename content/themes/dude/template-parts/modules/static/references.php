5b6ac7e2ce725689b8742fbbcf3f8fc9cc7762b3
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-18 14:39:08 +0300

    Generalized block-title and initial styles for services block

--- content/themes/dude/template-parts/modules/static/references.php
+++ content/themes/dude/template-parts/modules/static/references.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:49:22
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-18 14:15:46
+ * @Last Modified time: 2019-05-18 14:30:00
  *
  * @package dude2019
  */
@@ -12,6 +12,13 @@
 
 <section class="block block-references">
   <div class="container">
+
+    <header class="block-head">
+      <p class="block-title-pre" aria-describedby="latest-works">Latest wörks</p>
+      <h2 class="block-title" id="latest-works">Latest and greitest</h2>
+    </header>
+
+    <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder-reference.png" alt="Reference alt" />
 
   </div>
 </section>
