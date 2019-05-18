8a343a7011e2f06f8ae9ecade145d0ef9e23c757
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-18 19:04:22 +0300

    module: timeline

---
+++ content/themes/dude/template-parts/modules/timeline.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
+ * @Date:               2019-05-18 18:54:29
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2019-05-18 18:55:14
+ *
+ * @package dude2019
+ */
+
+include get_theme_file_path( 'template-parts/modules/static/timeline.php' );
