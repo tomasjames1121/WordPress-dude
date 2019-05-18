bd8139b5c13919033994fd8826bfc44fcc7a1bb5
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-18 15:30:51 +0300

    Arrow link and hover

--- content/themes/dude/template-parts/modules/static/references.php
+++ content/themes/dude/template-parts/modules/static/references.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:49:22
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-18 14:30:00
+ * @Last Modified time: 2019-05-18 15:17:00
  *
  * @package dude2019
  */
@@ -19,6 +19,20 @@
     </header>
 
     <img src="<?php echo get_template_directory_uri(); ?>/images/placeholder-reference.png" alt="Reference alt" />
+
+    <div class="cols cols-two">
+
+      <div class="col">
+        <?php include get_theme_file_path( '/svg/logo-sievo.svg' ); ?>
+        <h3 class="screen-reader-text">Sievo</h3>
+        <p>Kuten kaikki hyvät tarinat, alkaa tämäkin yhdestä twiitistä. Twiitistä joka johti Skype-palaveriin, reissuun isolle kirkolle ja lopulta jatkuvasti kehittyvän WordPress -sivuston toteuttamiseen...</p>
+
+        <p class="arrow-link-wrapper"><a href="#" class="arrow-link">Tsekkaa case<span class="arrow"></span></a></p>
+      </div>
+
+      <div class="col">
+      </div>
+    </div>
 
   </div>
 </section>
