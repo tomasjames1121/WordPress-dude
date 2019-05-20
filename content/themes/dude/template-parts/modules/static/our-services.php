417d7e5bf024f61288279749d6275946131babb0
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-20 19:10:43 +0300

    Responsiveness

--- content/themes/dude/template-parts/modules/static/our-services.php
+++ content/themes/dude/template-parts/modules/static/our-services.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:49:22
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-11 17:30:05
+ * @Last Modified time: 2019-05-20 18:52:18
  *
  * @package dude2019
  */
@@ -19,14 +19,14 @@
     </header>
 
     <div class="cols">
-      <div class="col">
+      <div class="col col-text">
         <h3>Verkkosivut ja -palvelut</h3>
         <p class="col-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ex consequat, facilisis.</p>
 
         <p class="cta-link"><a href="#">Tutustu</a></p>
       </div>
 
-      <div class="col">
+      <div class="col col-text">
         <h3>Visuaalinen suunnittelu</h3>
         <p class="col-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ex consequat, facilisis.</p>
 
