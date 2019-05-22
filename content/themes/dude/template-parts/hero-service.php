5e59476cf44ad00996548edf12c0f1e63f276199
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-22 20:05:33 +0300

    Rename $content => $herp_content

--- content/themes/dude/template-parts/hero-service.php
+++ content/themes/dude/template-parts/hero-service.php
@@ -2,13 +2,13 @@
 /**
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:14:20
- * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2019-05-18 16:41:23
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2019-05-22 20:05:23
  *
  * @package dude2019
  */
 
-$content = get_post_meta( get_the_id(), 'hero_content', true ); ?>
+$hero_content = get_post_meta( get_the_id(), 'hero_content', true ); ?>
 
 <section class="block block-hero block-hero-service">
   <div class="container">
