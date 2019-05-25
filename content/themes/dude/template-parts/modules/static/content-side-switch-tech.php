91e694cb240b4548e6dd1e0ab8fd1022f35f3a94
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-25 16:36:46 +0300

    Content side switch tech structure

--- content/themes/dude/template-parts/modules/static/content-side-switch-tech.php
+++ content/themes/dude/template-parts/modules/static/content-side-switch-tech.php
@@ -2,8 +2,8 @@
 /**
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-18 17:24:23
- * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2019-05-18 17:29:50
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2019-05-25 16:22:17
  *
  * @package dude2019
  */
@@ -12,38 +12,40 @@
 <section class="block block-content-side-switch">
   <div class="container">
 
-    <div class="col col-switch">
-      <ul>
-        <li><button class="active" data-tab="verkkosivut">Verkkosivut</button></li>
-        <li><button data-tab="verkkopalvelut">Verkkopalvelut</button></li>
-        <li><button data-tab="integraatiot">Integraatiot</button></li>
-      </ul>
-    </div>
+    <div class="cols">
 
-    <div class="col col-content">
-
-      <div class="content visible" data-tab="verkkosivut">
-        <p>Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
-
-        <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä. Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
-
-        <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä.</p>
+      <div class="col col-switch">
+        <ul>
+          <li><button class="active" data-tab="verkkosivut">Verkkosivut</button></li>
+          <li><button data-tab="verkkopalvelut">Verkkopalvelut</button></li>
+          <li><button data-tab="integraatiot">Integraatiot</button></li>
+        </ul>
       </div>
 
-      <div class="content" data-tab="verkkopalvelut">
-        <p>Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
+      <div class="col col-content">
+        <div class="content visible" data-tab="verkkosivut">
+          <p>Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
 
-        <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä. Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
+          <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä. Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
 
-        <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä.</p>
-      </div>
+          <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä.</p>
+        </div>
 
-      <div class="content" data-tab="integraatiot">
-        <p>Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
+        <div class="content" data-tab="verkkopalvelut">
+          <p>Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
 
-        <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä. Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
+          <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä. Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
 
-        <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä.</p>
+          <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä.</p>
+        </div>
+
+        <div class="content" data-tab="integraatiot">
+          <p>Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
+
+          <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä. Ilman hyviä verkkosivuja on vähän nolo olla. Sivuilla kävijät odottavat ensiluokkaista kokemusta ja parhaan kokemuksen tarjonnut yritys voittaa.</p>
+
+          <p>Olemme toteuttaneet monipuolisesti erilaisia verkkosivustoja kuten kansainvälisten yritysten sivustoja liidien kerääminen mielessä, tuote-esittelysivustoja monipuolisilla hakutoiminnoilla, markkinointisivustoja ja blogeja pienellä twistillä.</p>
+        </div>
       </div>
 
     </div>
