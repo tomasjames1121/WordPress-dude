d62e3852a8e944a834826d9659776ee294b42130
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-25 14:36:54 +0300

    visuaalinen suunnittelu hero

--- content/themes/dude/template-parts/hero.php
+++ content/themes/dude/template-parts/hero.php
@@ -2,8 +2,8 @@
 /**
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:05:23
- * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-20 20:21:50
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2019-05-25 14:14:16
  *
  * @package dude2019
  */
@@ -11,6 +11,17 @@
 if ( is_front_page() ) {
   include get_theme_file_path( 'template-parts/hero-fp.php' );
 } else if ( 9 === get_the_id() ) { // tech page
+  $logos = array(
+    'bauermedia',
+    'barexplosive',
+    'blackbruin',
+    'byemmi',
+    'crmservice',
+    'elonen',
+  );
+
+  include get_theme_file_path( 'template-parts/hero-service.php' );
+} else if ( 4485 === get_the_id() ) { // tech page
   $logos = array(
     'bauermedia',
     'barexplosive',
