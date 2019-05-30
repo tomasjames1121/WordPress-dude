e435304569926dbc3a0b5ce0ecbb1f85b2c3554b
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-30 15:33:50 +0300

    Preload support for regular hero

--- content/themes/dude/template-parts/hero-normal.php
+++ content/themes/dude/template-parts/hero-normal.php
@@ -3,13 +3,21 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:14:20
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-30 15:26:39
+ * @Last Modified time: 2019-05-30 15:33:34
  *
  * @package dude2019
  */
 
 $title = get_the_title();
 $content = get_post_meta( get_the_id(), 'hero_content', true );
+
+// Featured image
+$bg_image_tiny_default = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'tiny-preload-thumbnail' );
+$bg_image_tiny = $bg_image_tiny_default[0];
+$bg_image = null;
+if ( has_post_thumbnail() ) {
+  $bg_image = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
+}
 
 if ( is_post_type_archive( 'reference' ) ) {
   $title = 'Työt';
@@ -35,9 +43,12 @@ if ( is_post_type_archive( 'reference' ) ) {
 
     <div class="featured-image featured-image-side">
       <div class="shade"></div>
-      <div class="background-image preview lazyload" style="background-image: url('<?php echo get_the_post_thumbnail_url( get_the_id(), 'full' ) ?>" data-src="<?php echo get_the_post_thumbnail_url( get_the_id(), 'full' ) ?>"></div>
-      <div class="background-image full-image"<?php if ( preg_match( '/Windows Phone|Lumia|iPad/i', $_SERVER['HTTP_USER_AGENT'] ) ) : ?> style="background-image: url('<?php echo get_the_post_thumbnail_url( get_the_id(), 'full' ) ?>');"<?php endif; ?>></div>
-      <noscript><div class="background-image full-image" style="background-image: url('<?php echo get_the_post_thumbnail_url( get_the_id(), 'full' ) ?>');"></div></noscript>
+
+      <?php if ( $bg_image ) { ?>
+        <div class="background-image preview lazyload" style="background-image: url('<?php echo $bg_image_tiny; ?>');" data-src="<?php echo esc_url( $bg_image ); ?>"></div>
+        <div class="background-image full-image"<?php if ( preg_match( '/Windows Phone|Lumia|iPad/i', $_SERVER['HTTP_USER_AGENT'] ) ) : ?> style="background-image: url('<?php echo esc_url( $bg_image ); ?>');"<?php endif; ?>></div>
+        <noscript><div class="background-image full-image" style="background-image: url('<?php echo esc_url( $bg_image ); ?>');"></div></noscript>
+      <?php } ?>
     </div>
 
   </div>
