91a52228c3fd1f1e4aa40a7d7a89e3ae3c10cfc5
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-30 17:33:26 +0300

    Add timeline module styles

--- content/themes/dude/template-parts/modules/static/timeline.php
+++ content/themes/dude/template-parts/modules/static/timeline.php
@@ -2,8 +2,8 @@
 /**
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-18 18:54:41
- * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2019-05-18 19:04:17
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2019-05-30 17:32:34
  *
  * @package dude2019
  */
@@ -12,38 +12,40 @@
 <section class="block block-timeline">
   <div class="container">
 
-    <div class="col col-timeline">
+    <div class="columns">
+      <div class="column col col-timeline">
 
-      <div class="row" data-label="Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä." data-image="<?php echo get_theme_file_uri( 'images/timeline/risella.jpg' ) ?>">
-        <h2>2013</h2>
-        <p>Roni ja Juha perustavat yrityksen keväällä 2013 vähän niinkun vahingossa. Pitkiä päiviä sekä opettelua yrittäjyyden ihmeelliseen maailmaan.</p>
+        <div class="row" data-label="Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä." data-image="<?php echo get_theme_file_uri( 'images/timeline/risella.jpg' ) ?>">
+          <h2>2013</h2>
+          <p>Roni ja Juha perustavat yrityksen keväällä 2013 vähän niinkun vahingossa. Pitkiä päiviä sekä opettelua yrittäjyyden ihmeelliseen maailmaan.</p>
+        </div>
+
+        <div class="row" data-label="Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä." data-image="<?php echo get_theme_file_uri( 'images/timeline/risella.jpg' ) ?>">
+          <h2>2015</h2>
+          <p>Hohkavaara ja Dude yhdistyvät. Tätä juhlitaan parilla muumilimpparilla. Rock ’n roll haisee nokkaan.</p>
+        </div>
+
+        <div class="row" data-label="Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä." data-image="<?php echo get_theme_file_uri( 'images/timeline/risella.jpg' ) ?>">
+          <h2>2016</h2>
+          <p>Timi tulee mukaan völjyyn. Isompia ja vaativampia projekteja saadaan kotiutettua sekä kappas kummaa, myös tehtyä.</p>
+        </div>
+
+        <div class="row" data-label="Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä." data-image="<?php echo get_theme_file_uri( 'images/timeline/risella.jpg' ) ?>">
+          <h2>2018</h2>
+          <p>Liikevaihto ylitti 200 000. Soitettiin Get Lucky! Muutettiin Kauppakadulle.</p>
+        </div>
+
+        <div class="row" data-label="Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä." data-image="<?php echo get_theme_file_uri( 'images/timeline/risella.jpg' ) ?>">
+          <h2>2019</h2>
+          <p>Rekryttiin <a href="#">Henkka</a> ja pari muutakin rekryä tulilla. To be continued...</p>
+        </div>
+
       </div>
 
-      <div class="row" data-label="Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä." data-image="<?php echo get_theme_file_uri( 'images/timeline/risella.jpg' ) ?>">
-        <h2>2015</h2>
-        <p>Hohkavaara ja Dude yhdistyvät. Tätä juhlitaan parilla muumilimpparilla. Rock ’n roll haisee nokkaan.</p>
+      <div class="column col col-image">
+        <div class="label"><p>Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä.</p></div>
+        <img src="<?php echo get_theme_file_uri( 'images/timeline/risella.png' ) ?>" />
       </div>
-
-      <div class="row" data-label="Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä." data-image="<?php echo get_theme_file_uri( 'images/timeline/risella.jpg' ) ?>">
-        <h2>2016</h2>
-        <p>Timi tulee mukaan völjyyn. Isompia ja vaativampia projekteja saadaan kotiutettua sekä kappas kummaa, myös tehtyä.</p>
-      </div>
-
-      <div class="row" data-label="Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä." data-image="<?php echo get_theme_file_uri( 'images/timeline/risella.jpg' ) ?>">
-        <h2>2018</h2>
-        <p>Liikevaihto ylitti 200 000. Soitettiin Get Lucky! Muutettiin Kauppakadulle.</p>
-      </div>
-
-      <div class="row" data-label="Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä." data-image="<?php echo get_theme_file_uri( 'images/timeline/risella.jpg' ) ?>">
-        <h2>2019</h2>
-        <p>Rekryttiin <a href="#">Henkka</a> ja pari muutakin rekryä tulilla. To be continued...</p>
-      </div>
-
-    </div>
-
-    <div class="col col-image">
-      <div class="label"><p>Kristian virittelee nyrkkiä hanuriin. Kuva vänäriltä.</p></div>
-      <img src="<?php echo get_theme_file_uri( 'images/timeline/risella.png' ) ?>" />
     </div>
 
   </div>
