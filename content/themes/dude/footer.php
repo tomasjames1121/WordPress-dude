8a4f1db2535e902a67cee1d61c9b27e6fa6fc2eb
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-30 15:56:49 +0300

    add instant.page

--- content/themes/dude/footer.php
+++ content/themes/dude/footer.php
@@ -37,5 +37,8 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+
+<script src="//instant.page/1.2.2" type="module" integrity="sha384-2xV8M5griQmzyiY3CDqh1dn4z3llDVqZDqzjzcY+jCBCk/a5fXJmuZ/40JJAPeoU"></script>
+
 </body>
 </html>
