4060cc602e5f22745b8f99d2006d105e62a83958
tomasjames1121 <tomasjames1121@gmail.com>
2019-05-31 10:32:40 +0300

    Add code comment for recruitment

--- content/themes/dude/header.php
+++ content/themes/dude/header.php
@@ -11,7 +11,66 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?>>><script>
+/**
+ *
+ * 👋 Moi!
+ *
+ * Onpa mahtavaa, että sivustomme koodi on saanut sinut kiinnostumaan. Tuotannossa suurin osa on minifoituna,
+ * mutta meidän kaikki tekeminen löytyy myös GitHubistamme: https://github.com/digitoimistodude
+ *
+ * Tämän sivuston anti löytyy osoitteesta https://github.com/digitoimistodude/dude - tutki rauhassa!
+ *
+ * Haluaisitko meille töihin? Lähetä sähköpostia osoitteeseen moro@dude.fi.
+ * Avoimiin työpaikkoihimme voit tutustua osoitteessa https://www.dude.fi/tyopaikat
+ *
+ * Toivottavasti palaillaan!
+ *
+ *
+ *                    `/shhyyyhhyy+:`
+ *                 `+hh+.        `-+yds:
+ *         :`  ./+omo`                -sdo`
+ *         hd`dh/--.                    `+ms`
+ *        ydsMN                           `om:
+ *       +m` +m.                            -ms
+ *       /N:`                                `dy
+ *        -dNh+-..  :hyy+.                 -syyMs
+ *       `dy.   sNhsN/ `/hh:                /M.-+.
+ *       +N     mo -/     .sdo`             /M`
+ *       om    +N`          `+dy:           `M:
+ *       +m   -N:              -sds:`        +m.
+ *       om  :N/````              -ohyo:`     om-
+ *       ds +dhyysyyyyyyso/-.-:+oyyyyymNNmhs+:-/ms.
+ *     .hd``.          `.-/oys+:.`     ``..:/ohNymNy:
+ *     :/N+sN             :Nom+             /M.M: `.:/.
+ *      `M/sM+           `ms /N`           `mM.yd
+ *      /N`+MN:         -ms   sd-         -mdN .M-
+ *      hy -M/dy/-..-:ohy-     :yhs+/::/ohh-yh  hy
+ *     `M:  mo .:+ooo/-`         `.::///-` `N/  /N`
+ *     +N   :N.    .:+sssso/-`-+syyyyyso:` yd   `M:
+ *     mo    +m/-ohy+-.``.-/sho:.`    `-oddd`    mo
+ *    -M.     .+y/`                       :      yh
+ *    om                                         :M.
+ *    hy                                          sm`
+ *    N/.+                                       yyNd
+ *   :MdMs                                       -Msys
+ *   /-/N`                                        om-`
+ *     mo `                                      dhdy:
+ *    +Myhm                                      hy
+ *    `.`/N                                 :s+:`/N`
+ *       :M`-h                               +MoyhNo
+ *       .MdNd  -h`                      :o` `M:  `.
+ *       `y.my+hMo                        hNs:ds
+ *         -so/:M./s              --     --hNyys
+ *             -MdyM.   /:       ` ho`  .Mysyo.
+ *              s- dy-odM- .    yh /Mmo./N
+ *                 :Md::M+hM-  omsdyMs/ymy
+ *                  y  sh+.om`+N.  .:/:  `
+ *                          smN-
+ *                           y:
+ *
+ */
+</script>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
