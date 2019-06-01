d0a30b22bbe24bed0e9ab613f100f95f0960368d
tomasjames1121 <tomasjames1121@gmail.com>
2019-06-01 19:00:27 +0300

    blog author on footer

--- content/themes/dude/single.php
+++ content/themes/dude/single.php
@@ -7,8 +7,9 @@
  * @package dude
  */
 
-get_header(); ?>
+the_post();
 
+get_header(); ?>
 
 <div id="content" class="content-area">
 	<main role="main" id="main" class="site-main">
@@ -19,11 +20,47 @@ get_header(); ?>
 
     <section class="block block-single">
       <div class="container">
-        <?php while ( have_posts() ) {
-      	 the_post();
-				  get_template_part( 'template-parts/content', get_post_type() );
-			   }
-       ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+          <header class="block-head">
+            <p class="block-title-pre" aria-describedby="block-title-<?php echo sanitize_title( get_the_title() ) ?>"><?php echo ucfirst( date_i18n( 'l' ) ) ?>na, <?php echo get_the_date( 'j.n.Y' ) ?></p>
+            <h1 class="block-title" id="block-title-<?php echo sanitize_title( get_the_title() ) ?>"><?php the_title() ?></h1>
+          </header>
+
+          <div class="entry-content">
+            <?php the_content(); ?>
+          </div><!-- .entry-content -->
+
+          <div class="entry-author">
+            <?php // get author and person corresponding author
+            $user_id = get_the_author_meta( 'ID' );
+            $person_id = $wpdb->get_results(
+              $wpdb->prepare(
+                "SELECT post_id FROM {$wpdb->prefix}postmeta WHERE meta_key = %s AND meta_value = %d",
+                "email", get_the_author_meta( 'email' )
+              )
+            );
+
+            if ( ! is_wp_error( $person_id ) && ! empty( $person_id ) ) :
+              $job_title = get_post_meta( $person_id[0]->post_id, 'title', true );
+              $desc = get_post_meta( $person_id[0]->post_id, 'short_desc_blog', true ); ?>
+
+              <a href="<?php echo get_the_permalink( $user_id ) ?>"><?php echo get_avatar( $user_id, '512' ); ?></a>
+              <h3><?php echo get_the_author_meta( 'display_name' ); ?></h3>
+
+              <?php if ( ! empty( $job_title ) ) : ?>
+                <p><?php echo esc_html( $job_title ) ?></p>
+              <?php endif;
+
+              if ( ! empty( $desc ) ) : ?>
+                <p><?php echo esc_html( $desc ) ?></p>
+              <?php endif;
+
+            endif; ?>
+          </div>
+
+        </article><!-- #post-## -->
+
       </div>
     </section>
 
