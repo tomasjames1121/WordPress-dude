45c78638a1de7a639cabff3c7a00e43d955f1857
tomasjames1121 <tomasjames1121@gmail.com>
2019-06-01 19:40:32 +0300

    simpler gallery markup

--- content/themes/dude/template-parts/modules/gallery.php
+++ content/themes/dude/template-parts/modules/gallery.php
@@ -2,8 +2,8 @@
 /**
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-25 14:37:31
- * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-05-25 20:03:05
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2019-06-01 19:40:22
  *
  * @package dude2019
  */
@@ -23,6 +23,7 @@ if ( ! $images ) {
 
   foreach ( $images as $key => $image ) {
     $images[ $key ] = array(
+      'id'  => $image['ID'],
       'url' => $image['url'],
       'alt' => get_post_meta( $image['ID'], '_wp_attachment_image_alt', true ),
     );
@@ -34,29 +35,24 @@ if ( ! $images ) {
 
 if ( empty( $images ) ) {
   return;
-}
-
-// get images for the grid
-$visible_images = array_slice( $images, 0, 7 ); ?>
+} ?>
 
 <section class="block block-gallery">
   <div class="container">
 
     <div class="cols">
-      <?php foreach ( $visible_images as $image ) : ?>
-        <div class="col" style="background-image: url('<?php echo esc_url( $image['url'] ) ?>');">
-          <a class="global-link gallery-item" href="<?php echo esc_url( $image['url'] ) ?>"><span class="screen-reader-text">Avaa galleria kuvaan "<?php echo esc_html( $image['alt'] ) ?>"</span></a>
-        </div>
-      <?php endforeach; ?>
+      <?php $x = 0; foreach ( $images as $image ) :
+        if ( $x < 7 ) : ?>
+          <div id="<?php echo $image['id'] ?>" class="col" style="background-image: url('<?php echo esc_url( $image['url'] ) ?>');">
+            <a class="global-link gallery-item" href="<?php echo esc_url( $image['url'] ) ?>"><span class="screen-reader-text">Avaa galleria kuvaan "<?php echo esc_html( $image['alt'] ) ?>"</span></a>
+          </div>
+        <?php else: ?>
+          <figure id="<?php echo $image['id'] ?>" class="image hidden">
+            <a href="<?php echo esc_url( $image['url'] ) ?>" class="gallery-item"><?php echo $image['alt'] ?></a>
+          </figure>
+        <?php endif; ?>
+      <?php $x++; endforeach; ?>
     </div>
-
-    <?php
-    // This is for JS gallery use
-    foreach ( $images as $image ) : ?>
-      <figure class="image hidden">
-        <a href="<?php echo esc_url( $image['url'] ) ?>" class="gallery-item"><?php echo $image['alt'] ?></a>
-      </figure>
-    <?php endforeach; ?>
 
   </div>
 </section>
