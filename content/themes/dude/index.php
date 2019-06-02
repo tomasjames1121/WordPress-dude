bb8fa824971758d963e6d215a08806ba861734d2
tomasjames1121 <tomasjames1121@gmail.com>
2019-06-02 14:58:33 +0300

    Global links for blog grid posts

--- content/themes/dude/index.php
+++ content/themes/dude/index.php
@@ -117,6 +117,7 @@ get_header(); ?>
           <?php while( have_posts() ) : the_post(); ?>
             <div class="col">
               <div class="image has-lazyload">
+                <a class="global-link" href="<?php the_permalink() ?>"><span class="screen-reader-text"><?php the_title() ?></span></a>
                 <div class="background-image preview lazyload" style="background-image: url('<?php echo get_the_post_thumbnail_url( get_the_id(), 'tiny-preload-thumbnail' ) ?>');" data-src="<?php echo get_the_post_thumbnail_url( get_the_id(), 'large' ) ?>"></div>
                 <div class="background-image full-image"<?php if ( preg_match( '/Windows Phone|Lumia|iPad/i', $_SERVER['HTTP_USER_AGENT'] ) ) : ?> style="background-image: url('<?php echo get_the_post_thumbnail_url( get_the_id(), 'large' ) ?>');"<?php endif; ?>></div>
                 <noscript><div class="background-image full-image" style="background-image: url('<?php echo get_the_post_thumbnail_url( get_the_id(), 'large' ) ?>');"></div></noscript>
