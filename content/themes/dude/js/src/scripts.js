8ff014d44227c6eb18961d6ce8b646fdef653cea
tomasjames1121 <tomasjames1121@gmail.com>
2019-06-04 08:50:00 +0300

    The trick to viewport units on mobile

--- content/themes/dude/js/src/scripts.js
+++ content/themes/dude/js/src/scripts.js
@@ -6,6 +6,21 @@
 document.body.classList.remove('no-js');
 document.body.classList.add('js');
 
+// Correct Viewport Height on Mobile
+// Source: https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 // jQuery start
 ( function( $ ) {
 
