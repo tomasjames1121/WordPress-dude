4f6b3569bdea44d8d8f16e23742d3149eb932869
tomasjames1121 <tomasjames1121@gmail.com>
2019-06-06 15:38:08 +0300

    fp services text

--- content/themes/dude/template-parts/modules/static/our-services.php
+++ content/themes/dude/template-parts/modules/static/our-services.php
@@ -2,8 +2,8 @@
 /**
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:49:22
- * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-06-01 13:44:19
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2019-06-06 15:37:23
  *
  * @package dude2019
  */
@@ -22,22 +22,22 @@ $image_preload = wp_get_attachment_image_url( $image, 'tiny-preload-thumbnail' )
 
     <header class="block-head">
       <p class="block-title-pre" aria-describedby="block-title-our-services">Mitä me teemme?</p>
-      <h2 class="block-title" id="block-title-our-services">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+      <h2 class="block-title" id="block-title-our-services">Mieluiten vain muutamaa asiaa todella hyvin, kuin vähän kaikkea sinne päin.</h2>
     </header>
 
     <div class="cols">
       <div class="col col-text">
         <h3>Verkkosivut ja -palvelut</h3>
-        <p class="col-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ex consequat, facilisis.</p>
+        <p class="col-description">Ei valmisratkaisuja, vaan laadukkaasti räätälöiden. Tämähän se meidän ykkösjuttu on.</p>
 
-        <p class="cta-link"><a href="#">Tutustu</a></p>
+        <p class="cta-link"><a href="/verkkosivut">Tutustu</a></p>
       </div>
 
       <div class="col col-text">
         <h3>Visuaalinen suunnittelu</h3>
-        <p class="col-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ex consequat, facilisis.</p>
+        <p class="col-description">Verkkopalveluiden ulkoasujen lisäksi meillä tehdään myös maailmanluokan yritysilmeitä.</p>
 
-        <p class="cta-link"><a href="#">Tutustu</a></p>
+        <p class="cta-link"><a href="/visuaalinen-suunnittelu">Tutustu</a></p>
       </div>
 
       <div class="col col-featured-image">
