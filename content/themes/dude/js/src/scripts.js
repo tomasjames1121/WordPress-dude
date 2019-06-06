6ef1c842f18db758c054c712686938c5b2514f12
tomasjames1121 <tomasjames1121@gmail.com>
2019-06-06 10:48:23 +0300

    Fix vh on IE

--- content/themes/dude/js/src/scripts.js
+++ content/themes/dude/js/src/scripts.js
@@ -6,21 +6,6 @@
 document.body.classList.remove('no-js');
 document.body.classList.add('js');
 
-// Correct Viewport Height on Mobile
-// Source: https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 // Increase session page visits count
 sessionStorage.setItem( 'chat_greeting_visits', parseInt( sessionStorage.getItem( 'chat_greeting_visits' ) ) + 1 );
 
