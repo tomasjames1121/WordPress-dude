3b273e7c60fa8dfbecf84a9987b49902f4dfbe94
tomasjames1121 <tomasjames1121@gmail.com>
2019-06-06 23:36:57 +0300

    Contact page

--- content/themes/dude/template-parts/modules/wide-text.php
+++ content/themes/dude/template-parts/modules/wide-text.php
@@ -10,14 +10,30 @@
 
 $content = get_sub_field( 'content' );
 
-if ( empty( $content ) ) {
+if ( empty( $content ) && ! is_page(4487) ) {
   return;
 } ?>
 
-<section class="block block-wide-text">
+<section class="block block-wide-text<?php if ( is_page(4487) ) : ?> block-wide-text-contact<?php endif; ?>">
   <div class="container">
 
-    <?php echo wpautop( $content ); ?>
+    <?php if ( is_page(4487) ) :
+      echo gravity_form( 2, false, false, false, null, true, 49, true );
+    else :
+      echo wpautop( $content );
+    endif;
+    ?>
+
+    <?php if ( is_page(4487) ) : ?>
+      <div class="col-image">
+        <div class="image has-lazyload">
+          <div class="background-image preview lazyload" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/contact-tiny.jpg');" data-src="<?php echo get_template_directory_uri(); ?>/images/contact.jpg"></div>
+          <div class="background-image full-image"<?php if ( preg_match( '/Windows Phone|Lumia|iPad/i', $_SERVER['HTTP_USER_AGENT'] ) ) : ?> style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/contact.jpg');"<?php endif; ?>></div>
+          <noscript><div class="background-image full-image" style="background-image: url('<?php echo get_template_directory_uri(); ?>/images/contact.jpg');"></div></noscript>
+        </div>
+      </div>
+    <?php endif; ?>
+    
 
   </div>
 </section>
