de07cedc4eba9d5439ac5c7a14acb8806fc17b74
tomasjames1121 <tomasjames1121@gmail.com>
2019-06-07 14:57:51 +0300

    add person archive and redirect it to company page

---
+++ content/themes/dude/archive-person.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
+ * @Date:               2019-06-07 14:56:04
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2019-06-07 14:56:24
+ *
+ * @package dude2019
+ */
+
+wp_safe_redirect( 'https://www.dude.fi/yritys' );
