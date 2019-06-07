5bb8101032c08c5de7721adf46c6a98d4994a8eb
tomasjames1121 <tomasjames1121@gmail.com>
2019-06-07 12:18:01 +0300

    fix links in suggestes articles

--- content/themes/dude/templates/relevanssi-related.php
+++ content/themes/dude/templates/relevanssi-related.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-07 14:52:34
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2019-06-03 11:06:23
+ * @Last Modified time: 2019-06-07 12:17:41
  *
  * @package bauermedia
  */
@@ -24,13 +24,13 @@ if ( empty( $related_posts ) ) {
       <?php foreach ( $related_posts as $related_post ) : ?>
         <div class="col">
           <div class="image has-lazyload">
-            <a class="global-link" href="<?php the_permalink() ?>"><span class="screen-reader-text"><?php the_title() ?></span></a>
+            <a class="global-link" href="<?php the_permalink() ?>"><span class="screen-reader-text"><?php echo get_the_title( $related_post ) ?></span></a>
             <div class="background-image preview lazyload" style="background-image: url('<?php echo get_the_post_thumbnail_url( $related_post, 'tiny-preload-thumbnail' ) ?>');" data-src="<?php echo get_the_post_thumbnail_url( $related_post, 'large' ) ?>"></div>
             <div class="background-image full-image"<?php if ( preg_match( '/Windows Phone|Lumia|iPad/i', $_SERVER['HTTP_USER_AGENT'] ) ) : ?> style="background-image: url('<?php echo get_the_post_thumbnail_url( $related_post, 'large' ) ?>');"<?php endif; ?>></div>
             <noscript><div class="background-image full-image" style="background-image: url('<?php echo get_the_post_thumbnail_url( $related_post, 'large' ) ?>');"></div></noscript>
           </div>
 
-          <h3><a href="<?php the_permalink( $related_post ) ?>"><?php echo get_the_title( $related_post ) ?></a></h3>
+          <h3><a href="<?php echo get_the_permalink( $related_post ) ?>"><?php echo get_the_title( $related_post ) ?></a></h3>
           <p class="date"><?php echo ucfirst( date_i18n( 'l', get_the_date( 'U' ) ) ) ?>na, <?php echo get_the_date( 'j.n.Y', $related_post ) ?></p>
         </div>
       <?php endforeach; ?>
