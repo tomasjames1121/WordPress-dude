fec08e2c2587f95f5d7a11f76362f11bdbc97bcc
tomasjames1121 <tomasjames1121@gmail.com>
2019-06-25 17:51:08 +0300

    Load larger image to mobile hero single

--- content/themes/dude/template-parts/hero-single.php
+++ content/themes/dude/template-parts/hero-single.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:14:20
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2019-06-25 13:38:59
+ * @Last Modified time: 2019-06-25 17:50:30
  *
  * @package dude2019
  */
@@ -16,7 +16,7 @@ if ( ! empty( $title_alt ) ) {
 
 // Featured image
 $bg_image_tiny_default = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'tiny-preload-thumbnail' );
-$bg_image_mobile = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+$bg_image_mobile = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' );
 $bg_image_tiny = $bg_image_tiny_default[0];
 $bg_image = null;
 if ( has_post_thumbnail() ) {
