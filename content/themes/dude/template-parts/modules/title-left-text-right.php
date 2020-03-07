6abc2c82c9990ca30072f36c87b6865d0fdbaa7c
tomasjames1121 <tomasjames1121@gmail.com>
2020-03-07 14:34:09 +0200

    Title left text right module

--- content/themes/dude/template-parts/modules/title-left-text-right.php
+++ content/themes/dude/template-parts/modules/title-left-text-right.php
@@ -3,7 +3,7 @@
  * @Author:             Roni Laukkarinen, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:48:37
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2020-03-07 13:16:30
+ * @Last Modified time: 2020-03-07 14:33:10
  *
  * @package dude
  */
@@ -11,6 +11,20 @@
 
 <section class="block block-title-left-text-right">
   <div class="container">
+
+    <div class="cols">
+      <div class="col col-title">
+        <h2>Me teemme kaikenlaista paskaa, että tuota siinä sopii miettiä.</h2>
+      </div>
+
+      <div class="col col-text">
+        <p>Dudella on laadukkaan työnjäljen lisäksi myös rikasta ideointikykyä. Olemme parhaimmillamme, kun saamme jalostaa asiakkaamme ideoita palvelemaan loppukäyttäjää parhaimmalla mahdollisella tavalla.</p>
+
+        <p>Haluamme olla omalta osaltamme kehittämässä asiakkaamme brändiä ja imagoa, oli sitten kyseessä verkkosivustouudistus tai brändi-ilmeen päivitys. Kaikessa tekemisessämme korostuu tiivis yhteistyö, laadukas suunnittelu sekä millintarkka tekninen toteutus.</p>
+
+        <p class="link-wrapper"><a href="#">Aloitetaan projekti</a></p>
+      </div>
+    </div>
 
   </div>
 </section>
