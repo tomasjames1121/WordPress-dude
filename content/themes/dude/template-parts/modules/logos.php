49d89dad989b3daa1d6eeb873f17216ee7184268
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-15 19:35:46 +0300

    Logo wall, tweaks

---
+++ content/themes/dude/template-parts/modules/logos.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @Author:             Roni Laukkarinen, Digitoimisto Dude Oy (https://dude.fi)
+ * @Date:               2020-07-15 18:34:29
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2020-07-15 19:28:22
+ *
+ * @package dude
+ */
+?>
+
+<section class="block block-logos">
+  <div class="container">
+
+    <header class="block-head block-head-small">
+      <h2 class="block-title">Meihin luottavat</h2>
+    </header>
+
+    <div class="logo-wall">
+      <div class="logo-tulos"><?php include get_theme_file_path( '/svg/logos/tulos.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/bitwise.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/bauermedia.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/fimpec.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/nilkko.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/nodeon.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/paahtimopapu.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/northpatrol.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/hotellialba.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/varjola-alt.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/crmservice.svg' ); ?></div>
+      <div class="logo-elonen"><?php include get_theme_file_path( '/svg/logos/elonen.svg' ); ?></div>
+    </div>
+
+  </div>
+</section>
