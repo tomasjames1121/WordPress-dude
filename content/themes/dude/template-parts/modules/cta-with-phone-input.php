fda0d49039c92233bbd4b6e492dca04a8a0d1fea
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-15 16:34:06 +0300

    CTA with phone input

--- content/themes/dude/template-parts/modules/cta-with-phone-input.php
+++ content/themes/dude/template-parts/modules/cta-with-phone-input.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-18 19:33:53
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2020-07-15 15:18:37
+ * @Last Modified time: 2020-07-15 15:53:41
  *
  * @package dude
  */
@@ -12,14 +12,18 @@
 <section class="block block-cta-with-phone-input">
   <div class="container">
 
-    <h2>Kyllä, naurettavan hyvännäköistä.</h2>
-    <p>Jos tuntemukset ovat kutakuinkin tuota luokkaa, niin jätäthän meille soittopyynnön. Meidän Kristian soittaa pikimmiten perään.</p>
+    <div class="col">
+      <h2>Kyllä, naurettavan hyvännäköistä.</h2>
+      <p>Jos tuntemukset ovat kutakuinkin tuota luokkaa, niin jätäthän meille soittopyynnön. Meidän Kristian soittaa pikimmiten perään.</p>
+    </div>
 
-    <?php
-      if ( function_exists( 'gravity_form' ) ) {
-        gravity_form( 1, $display_title = false, $display_description = false, $display_inactive = false, $field_values = null, $ajax = false, $echo = true );
-      }
-    ?>
+    <div class="col">
+      <?php
+        if ( function_exists( 'gravity_form' ) ) {
+          gravity_form( 1, $display_title = false, $display_description = false, $display_inactive = false, $field_values = null, $ajax = false, $echo = true );
+        }
+      ?>
+    </div>
 
   </div>
 </section>
