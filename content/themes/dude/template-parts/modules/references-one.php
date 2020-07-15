ca9b219a69c7344e2a7db6f8e5f57de240ca5c25
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-15 12:12:44 +0300

    General improvements, refrences one

--- content/themes/dude/template-parts/modules/references-one.php
+++ content/themes/dude/template-parts/modules/references-one.php
@@ -2,8 +2,8 @@
 /**
  * @Author:             Roni Laukkarinen, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:50:23
- * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2020-07-14 14:28:13
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2020-07-15 12:00:57
  *
  * @package dude
  */
@@ -11,56 +11,40 @@
 $reference = [];
 $reference_id = get_sub_field( 'selected_reference' );
 
-if ( empty( $reference_id ) || ! post_exists_id( $reference_id ) ) {
+if ( empty( $reference_id ) ) {
   return;
 }
-
-$thumbnail_id = get_post_meta( $reference_id, 'frontpage_upsell_image', true );
-$title = get_post_meta( $reference_id, 'frontpage_upsell_title', true );
-$description = get_post_meta( $reference_id, 'frontpage_upsell_desc', true );
-
-if ( empty( $thumbnail_id ) ) {
-  return;
-}
-
-/**
- * NB! This is needed in case references.php module is in the same page.
- * $big_reference_ids variable is used there to prevent showing same reference
- * twice on the same page.
- */
-$big_reference_ids[] = $reference_id;
 
 $reference = array(
   'id'                => $reference_id,
   'upper_title'       => get_the_title( $reference_id ),
   'title'             => ( ! empty( $title ) ) ? $title : get_the_title( $reference_id ),
-  'thumbnail_id'      => $thumbnail_id,
   'excerpt'           => ( ! empty( $description ) ) ? $description : get_the_excerpt( $reference_id ),
   'permalink'         => get_the_permalink( $reference_id ),
+  'frontpage_upsell_image' => get_field( 'frontpage_upsell_image', $reference_id ),
+  'frontpage_upsell_title' => get_field( 'frontpage_upsell_title', $reference_id ),
+  'frontpage_upsell_desc' => get_field( 'frontpage_upsell_desc', $reference_id ),
+  'image_skew' => get_field( 'image_skew', $reference_id ),
 ); ?>
 
 <section class="block block-references-one">
   <div class="container">
 
-    <header class="block-head screen-reader-text">
-      <h2 class="block-title" id="block-title-our-services">Referenssit</h2>
-    </header>
-
     <div class="cols cols-two">
 
       <div class="col col-content">
-        <p aria-describedby="$reference['title']"><?php echo esc_html( $reference['upper_title'] ); ?></p>
-        <h3 id="<?php echo esc_attr( $reference['title'] ) ?>"><?php echo esc_html( $reference['title'] ); ?></h3>
-        <p><?php echo $reference['excerpt']; // phpcs:ignore ?></p>
-        <p class="arrow-link-wrapper"><a href="<?php echo esc_html( $reference['permalink'] ) ?>" class="arrow-link">Tutustu työhön<span class="arrow"></span></a></p>
+        <p aria-describedby="<?php echo esc_html( sanitize_title( $reference['frontpage_upsell_title'] ) ); ?>"><?php echo esc_html( $reference['upper_title'] ); ?></p>
+        <h2 class="block-title" id="<?php echo esc_html( sanitize_title( $reference['frontpage_upsell_title'] ) ); ?>"><?php echo esc_html( $reference['frontpage_upsell_title'] ); ?></h2>
+        <?php echo wpautop( $reference['frontpage_upsell_desc'] ); // phpcs:ignore ?>
+
+        <p class="button-wrapper"><a href="<?php echo esc_html( $reference['permalink'] ) ?>" class="button button-glitch button-mint">Tutustu työhön<?php include get_theme_file_path( '/svg/arrow-right.svg' ); ?></a></p>
       </div>
 
       <div class="col col-reference-image col-reference-image-main">
-        <?php image_lazyload_div( $reference['thumbnail_id'] ); ?>
+        <?php image_lazyload_div( $reference['image_skew']['ID'] ); ?>
       </div>
 
     </div>
 
   </div>
 </section>
-
