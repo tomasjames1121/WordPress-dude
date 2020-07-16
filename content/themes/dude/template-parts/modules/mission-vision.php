c7de9f325513e92a5ecd14052d8aeba9c1b22d23
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-16 18:42:15 +0300

    Mission vision, values, etc

---
+++ content/themes/dude/template-parts/modules/mission-vision.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @Author: Roni Laukkarinen
+ * @Date:   2020-07-16 17:32:53
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2020-07-16 17:46:04
+ * @package dude
+ */
+?>
+
+<section class="block block-mission-vision">
+  <div class="container">
+    <div class="cols cols-two">
+
+      <div class="col">
+        <div>
+          <svg aria-hidden="true" height="103" viewBox="0 0 104 103" width="104" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"><path d="m1 1h101.099v101h-101.099z"/><path d="m19.549 20h64v63h-64z"/><path d="m37.549 38h28v27h-28z"/></g></svg>
+        </div>
+
+        <div>
+          <h2>Missiomme</h2>
+          <p>Suunnittelemme ja jalostamme asiakkaidemme ideoista verkkopalveluita, joilla uskomme olevan merkitystä yhteiskunnassamme. Laatu ja kokemus suunnittelussa sekä teknisessä toteutuksessa ovat yrityksemme kruunun jalokivet.</p>
+        </div>
+      </div>
+
+      <div class="col">
+        <div>
+          <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="142" height="103" viewBox="0 0 142 103"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"><path d="M71 2.012L128.275 102H13.725L71 2.012z"/><path d="M71 33c25.534 0 48.68 8.672 69.444 26.003C119.678 76.329 96.532 85 71 85c-25.534 0-48.68-8.672-69.444-26.003C22.322 41.671 45.468 33 71 33z"/><circle cx="71" cy="59" r="13"/></g></svg>
+        </div>
+
+        <div>
+          <h2>Visiomme</h2>
+          <p>Tavoitteenamme on kasvaa vakaasti ja hallitusti tuottaen asiakkaillemme laadukkaita verkkosivuja, verkkopalveluita ja yritysilmeitä. Haluamme nähdä asiakkaidemme liikevaihdon ja tuloksen kasvavan ansiostamme.</p>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</section>
