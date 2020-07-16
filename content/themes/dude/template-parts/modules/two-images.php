fc9d2e0f10c80a0ff05e34f54d5b07e92024b90b
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-16 14:38:12 +0300

    Two images module

--- content/themes/dude/template-parts/modules/two-images.php
+++ content/themes/dude/template-parts/modules/two-images.php
@@ -2,8 +2,8 @@
 /**
  * @Author: Timi Wahalahti
  * @Date:   2020-07-14 15:02:12
- * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2020-07-14 15:04:00
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2020-07-16 14:28:07
  * @package dude
  */
 
@@ -16,7 +16,9 @@ if ( empty( $image_left ) || empty( $image_right ) ) {
 
 <section class="block block-two-images">
   <div class="container">
-    <div class="col"><?php image_lazyload_div( $image_left ) ?></div>
-    <div class="col"><?php image_lazyload_div( $image_right ) ?></div>
+    <div class="cols cols-two">
+      <div class="col"><?php image_lazyload_div( $image_left ); ?></div>
+      <div class="col"><?php image_lazyload_div( $image_right ); ?></div>
+    </div>
   </div>
 </section>
