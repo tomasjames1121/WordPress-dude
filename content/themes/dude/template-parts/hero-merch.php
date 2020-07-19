3d26ca5db799f2f009e50659d57960033b3299fd
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-19 14:48:27 +0300

    Merch

--- content/themes/dude/template-parts/hero-merch.php
+++ content/themes/dude/template-parts/hero-merch.php
@@ -3,16 +3,10 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:14:20
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2020-07-16 16:14:05
+ * @Last Modified time: 2020-07-19 14:24:20
  *
  * @package dude
  */
-
-// Featured image
-$bg_image_tiny_default = get_template_directory_uri() . '/images/merch-tiny.jpg';
-$bg_image_mobile = $bg_image_tiny_default;
-$bg_image_tiny = $bg_image_tiny_default;
-$bg_image = get_template_directory_uri() . '/images/merch.jpg';
 
 $title = 'Merch';
 $content = '<p>Haluatko olla oman elämäsi Dude tai Dudette? Koetko vahvaa tunnesidettä  kulttiheimoon nimeltä Dude? Vai kaipaatko muuten vaan helevetin hyvännäköistä vaatetta, jota ei ihan joka Seppälästä löydy? Sama se meille, kunhan pidät niitä rinta rottingilla ja hyvällä asenteella.</p>
@@ -20,7 +14,7 @@ $content = '<p>Haluatko olla oman elämäsi Dude tai Dudette? Koetko vahvaa tunn
 <p class="details">Dude maksaa postarit, jos lähetellään Suomen sisällä. Toimitusajat riippuu ihan siitä miten me jaksetaan näitä pakkailla ja postitella. Nämä on sen verran tyylikästä, että palautella ei tartte - eikä otettais vastaankaan. Jos kysyttävää niin heitä viestiä vaikka <a href="#" class="open-chat">chatin avulla tästä</a> tai sähköpostilla <a href="mailto:moro@dude.fi">moro@dude.fi</a>.</p>';
 ?>
 
-<section class="block block-hero-merch block-hero block-hero-side-columns">
+<section class="block block-hero-merch block-hero block-hero-reference is-centered">
   <div class="container">
 
     <div class="content">
@@ -33,15 +27,6 @@ $content = '<p>Haluatko olla oman elämäsi Dude tai Dudette? Koetko vahvaa tunn
         ?>
       </div>
     </div>
-
-    <?php if ( $bg_image ) { ?>
-    <div class="featured-image featured-image-side">
-      <div class="shade"></div>
-        <div class="background-image preview lazyload" style="background-image: url('<?php echo esc_url( $bg_image ); ?>');" data-src="<?php echo esc_url( $bg_image ); ?>" data-src-mobile="<?php echo esc_url( $bg_image ); ?>"></div>
-        <div class="background-image full-image"<?php if ( preg_match( '/Windows Phone|Lumia|iPad|Safari/i', $_SERVER['HTTP_USER_AGENT'] ) ) : ?> style="background-image: url('<?php echo esc_url( $bg_image ); ?>');"<?php endif; ?>></div>
-        <noscript><div class="background-image full-image" style="background-image: url('<?php echo esc_url( $bg_image ); ?>');"></div></noscript>
-    </div>
-    <?php } ?>
 
   </div>
 </section>
