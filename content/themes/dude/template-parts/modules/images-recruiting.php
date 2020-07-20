0f48fc1bc3be8e9ddddc4d3c2626db2d78bf2d3c
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-20 16:20:51 +0300

    Recruiting single page, fixes

---
+++ content/themes/dude/template-parts/modules/images-recruiting.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @Author:             Roni Laukkarinen, Digitoimisto Dude Oy (https://dude.fi)
+ * @Date:               2020-03-11 15:09:52
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2020-07-20 16:18:32
+ *
+ * @package dude
+ */
+?>
+
+<section class="block block-community-photos block-photos-recruiting">
+  <div class="container">
+    <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/rekry-1.jpg" alt="" /></div>
+    <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/rekry-2.jpg" alt="" /></div>
+    <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/rekry-3.jpg" alt="" /></div>
+    <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/rekry-4.jpg" alt="" /></div>
+    <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/rekry-6.jpg" alt="" /></div>
+    <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/rekry-7.jpg" alt="" /></div>
+    <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/rekry-9.jpg" alt="" /></div>
+    <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/rekry-10.jpg" alt="" /></div>
+    <div class="photo"><img src="<?php echo get_template_directory_uri(); ?>/images/rekry-11.jpg" alt="" /></div>
+  </div>
+ </section>
