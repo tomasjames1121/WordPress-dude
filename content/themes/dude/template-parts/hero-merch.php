301e278ceddefd16ce76aec69ff4f15879025946
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-22 16:51:37 +0300

    Merch fixes

--- content/themes/dude/template-parts/hero-merch.php
+++ content/themes/dude/template-parts/hero-merch.php
@@ -11,7 +11,7 @@
 $title = 'Merch';
 $content = '<p>Haluatko olla oman elämäsi Dude tai Dudette? Koetko vahvaa tunnesidettä  kulttiheimoon nimeltä Dude? Vai kaipaatko muuten vaan helevetin hyvännäköistä vaatetta, jota ei ihan joka Seppälästä löydy? Sama se meille, kunhan pidät niitä rinta rottingilla ja hyvällä asenteella.</p>
 
-<p class="details">Dude maksaa postarit, jos lähetellään Suomen sisällä. Toimitusajat riippuu ihan siitä miten me jaksetaan näitä pakkailla ja postitella. Nämä on sen verran tyylikästä, että palautella ei tartte - eikä otettais vastaankaan. Jos kysyttävää niin heitä viestiä vaikka <a href="#" class="open-chat">chatin avulla tästä</a> tai sähköpostilla <a href="mailto:moro@dude.fi">moro@dude.fi</a>.</p>';
+<p class="details">Dude maksaa postarit, jos lähetellään Suomen sisällä. Toimitusajat riippuu ihan siitä miten me jaksetaan näitä pakkailla ja postitella. Nämä on sen verran tyylikästä, että palautella ei tartte - eikä otettais vastaankaan. Jos kysyttävää niin heitä viestiä vaikka <a href="#" class="open-chat no-text-link">chatin avulla tästä</a> tai sähköpostilla <a href="mailto:moro@dude.fi" class="no-text-link">moro@dude.fi</a>.</p>';
 ?>
 
 <section class="block block-hero-merch block-hero block-hero-reference is-centered">
