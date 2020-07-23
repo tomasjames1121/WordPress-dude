e7ce6a9fc61f5415f9368350939ffa798f2e653c
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-23 12:13:08 +0300

    Thanks page

--- content/themes/dude/template-parts/hero-contact-thanks.php
+++ content/themes/dude/template-parts/hero-contact-thanks.php
@@ -3,7 +3,7 @@
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:14:20
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2020-07-16 16:05:36
+ * @Last Modified time: 2020-07-23 12:10:58
  *
  * @package dude
  */
@@ -11,16 +11,19 @@
 ?>
 
 <section class="block block-hero block-hero-contact-thanks">
+
+    <div class="shade"></div>
+    <div class="vimeo-wrapper">
+      <iframe data-no-lazy="1" id="video" src="https://player.vimeo.com/video/9778852?autoplay=1&loop=1&title=0&byline=0&portrait=0&muted=1#t=24s" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay; fullscreen" title="Mr Oizo - Flat beat"></iframe><script src="https://player.vimeo.com/api/player.js"></script>
+    </div>
+
   <div class="container">
 
-    <h1>Kiitos yhteydenotosta!</h1>
+    <div class="content">
+      <h1>Kiitos yhteydenotosta!</h1>
 
-    <div class="contact-wrap">
-      <div class="content">
-        <p>Myyntiosastomme käsittelee viestiäsi parhaillaan...</p>
-
-        <div style="padding:61.25% 0 0 0;position:relative;"><iframe data-no-lazy="1" id="video" src="https://player.vimeo.com/video/9778852?autoplay=1&loop=1&title=0&byline=0&portrait=0&muted=1#t=24s" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" title="Mr Oizo - Flat beat" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
-
+      <div class="hero-description">
+        <p><a class="no-text-link" href="https://www.youtube.com/watch?v=qmsbP13xu6k">Myyntiosastomme</a> käsittelee viestiäsi parhaillaan ja palaa asiaan pian!</p>
       </div>
 
     </div>
