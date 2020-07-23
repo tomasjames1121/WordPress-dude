fb49b4113ca6457c2e3ea6f3081b1a89b2c3fb80
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-23 16:05:28 +0300

    Update surveys for new brand

--- content/themes/dude/template-surveys.php
+++ content/themes/dude/template-surveys.php
@@ -88,7 +88,8 @@ $form_description = get_field( 'form_description' );
 
         <div class="splash">
           <header class="survey-header">
-            <img src="<?php echo get_template_directory_uri(); ?>/images/survey-logo.png" alt="Nyt voi antaa palautetta" />
+            <?php include get_theme_file_path( '/svg/logo.svg' ); ?>
+
 
             <?php if ( ! empty( $form_title ) ) : ?>
               <h1><?php echo $form_title; ?></h1>
