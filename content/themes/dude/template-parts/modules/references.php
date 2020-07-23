18128b04a1b1e3ae16f368681050cc4d04a756de
tomasjames1121 <tomasjames1121@gmail.com>
2020-07-23 19:54:41 +0300

    Fix slider lazy load problem

--- content/themes/dude/template-parts/modules/references.php
+++ content/themes/dude/template-parts/modules/references.php
@@ -2,8 +2,8 @@
 /**
  * @Author:             Timi Wahalahti, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2019-05-10 16:50:23
- * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2020-07-23 16:34:44
+ * @Last Modified by:   Roni Laukkarinen
+ * @Last Modified time: 2020-07-23 19:54:14
  *
  * @package dude
  */
@@ -37,12 +37,9 @@ if ( ! $references ) {
 		  $references[] = array(
 			'id'                => get_the_id(),
 			'title'             => get_the_title(),
-			'image_preload_url' => get_the_post_thumbnail_url( get_the_id(), 'tiny-preload-thumbnail' ),
-			'image_url'         => get_the_post_thumbnail_url( get_the_id(), 'large' ),
-			'image_url_mobile'  => get_the_post_thumbnail_url( get_the_id(), 'large' ),
+      'image_id'          => get_post_thumbnail_id( get_the_id() ),
 			'excerpt'           => get_post_meta( get_the_id(), 'short_desc', true ),
       'permalink'         => get_the_permalink(),
-      'alt_image_listings_preload' => get_field( 'alt_image_listings', get_the_id() )['sizes']['tiny-preload-thumbnail'],
       'alt_image_listings' => get_field( 'alt_image_listings', get_the_id() ),
 		  );
 			}
@@ -67,11 +64,9 @@ if ( ! $references ) {
 
       <?php foreach ( $references as $reference ) :
           if ( ! empty( $reference['alt_image_listings'] ) ) :
-            $feat_image_tiny = $reference['alt_image_listings_preload'];
-            $feat_image = $reference['alt_image_listings']['url'];
+            $feat_image = $reference['alt_image_listings']['id'];
           else :
-            $feat_image_tiny = $reference['image_preload_url'];
-            $feat_image = $reference['image_url'];
+            $feat_image = $reference['image_id'];
           endif;
         ?>
         <div class="reference">
@@ -79,9 +74,7 @@ if ( ! $references ) {
 
           <div class="reference-image">
             <div class="image" aria-hidden="true">
-              <div class="background-image preview lazyload" style="background-image: url('<?php echo $feat_image_tiny; ?>');" data-src="<?php echo $feat_image; ?>" data-src-mobile="<?php echo $feat_image_tiny; ?>" aria-hidden="true"></div>
-              <div aria-hidden="true" class="background-image full-image"<?php if ( preg_match( '/Windows Phone|Lumia|iPad|Safari/i', $_SERVER['HTTP_USER_AGENT'] ) ) : // phpcs:ignore ?> style="background-image: url('<?php echo $feat_image; ?>');"<?php endif; ?>></div>
-              <noscript><div aria-hidden="true" class="background-image full-image" style="background-image: url('<?php echo $feat_image; ?>');"></div></noscript>
+              <?php image_lazyload_div( $feat_image ); ?>
             </div>
 
             <div class="reference-content">
