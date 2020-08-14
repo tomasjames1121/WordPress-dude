c6ac595d74cc8b6a4043675218245b9e9dbcc8c1
tomasjames1121 <tomasjames1121@gmail.com>
2020-08-14 10:23:50 +0300

    Recruiting content

--- content/themes/dude/template-parts/modules/jobs.php
+++ content/themes/dude/template-parts/modules/jobs.php
@@ -3,7 +3,7 @@
  * @Author: 						Roni Laukkarinen
  * @Date:   						2020-07-18 19:03:51
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2020-07-23 14:08:40
+ * @Last Modified time: 2020-08-14 09:56:24
  *
  * @package dude
  */
@@ -15,13 +15,25 @@
 
     <div class="job-box">
       <div class="col">
-        <h2>WordPress-kehittäjä</h2>
-        <p>Jyväskylä</p>
+        <h2>Front-end kehittäjä</h2>
+        <p>Jyväskylä tai etänä</p>
       </div>
 
       <div class="col col-apply">
         <p>Haku voimassa toistaiseksi</p>
         <p class="button-wrapper"><a class="button button-glitch" href="<?php echo esc_url( get_page_link( 6117 ) ); ?>">Hae paikkaa<?php include get_theme_file_path( '/svg/arrow-right.svg' ); ?></a></p>
+      </div>
+    </div>
+
+    <div class="job-box">
+      <div class="col">
+        <h2>Avoin hakemus</h2>
+        <p>Jyväskylä tai etänä</p>
+      </div>
+
+      <div class="col col-apply">
+        <p>Haku voimassa toistaiseksi</p>
+        <p class="button-wrapper"><a class="button button-glitch" href="mailto:moro@dude.fi">Hae paikkaa<?php include get_theme_file_path( '/svg/arrow-right.svg' ); ?></a></p>
       </div>
     </div>
 
