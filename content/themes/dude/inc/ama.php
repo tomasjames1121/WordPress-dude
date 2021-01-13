3944e135908c9d0312ce29d11e2315bca0207fe7
tomasjames1121 <tomasjames1121@gmail.com>
2021-01-13 11:12:58 +0200

    custom api endpoint to get number of ama drafts

--- content/themes/dude/inc/ama.php
+++ content/themes/dude/inc/ama.php
@@ -3,8 +3,25 @@
  * @Author: Timi Wahalahti
  * @Date:   2021-01-13 10:34:51
  * @Last Modified by:   Timi Wahalahti
- * @Last Modified time: 2021-01-13 10:53:21
+ * @Last Modified time: 2021-01-13 11:12:27
  */
+
+add_action( 'rest_api_init', function () {
+  register_rest_route( 'ama/v1', '/drafts', array(
+    'methods'   => 'GET',
+    'callback'  => 'dude_get_ama_drafts',
+  ) );
+} );
+
+function dude_get_ama_drafts( $data ) {
+  $count = wp_cache_get( 'ama-drafts', 'theme' );
+  if ( ! $count ) {
+    $count = wp_count_posts( 'ama' )->draft;
+    wp_cache_set( 'ama-drafts', 'theme', $count, MINUTE_IN_SECONDS / 4 );
+  }
+
+  return $count;
+} // end dude_get_ama_drafts
 
 function dude_get_ama_entry( $post_id ) {
   $question = get_the_title( $post_id );
@@ -18,4 +35,4 @@ function dude_get_ama_entry( $post_id ) {
     <?php echo wp_kses_post( wpautop( $answer ) ); ?>
   </div>
   <?php return ob_get_clean();
-}
+} // end dude_get_ama_entry
