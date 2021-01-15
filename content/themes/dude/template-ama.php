b5ca5f4995b1511560a7b5bb4883581e47b70b0d
tomasjames1121 <tomasjames1121@gmail.com>
2021-01-15 18:30:47 +0200

    Fix label

--- content/themes/dude/template-ama.php
+++ content/themes/dude/template-ama.php
@@ -150,7 +150,7 @@ $questions = array_reverse( $questions );
           <?php else : ?>
 
           <div class="form">
-            <p><b>AMA on päättynyt!</b> Kiitos kysymyksistänne.</p>
+            <p><b>AMA on päättynyt!</b> Kiitos kysymyksistä, niitä tuli enemmän kuin odotimme!</p>
           </div>
 
           <?php endif; ?>
