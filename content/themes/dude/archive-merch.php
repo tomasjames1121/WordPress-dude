c3c3df3672268d2c7e4fe0ac3fceddd7dd3954d6
tomasjames1121 <tomasjames1121@gmail.com>
2021-09-29 10:36:32 +0300

    Update merch photos, fix button flex issue

--- content/themes/dude/archive-merch.php
+++ content/themes/dude/archive-merch.php
@@ -183,11 +183,11 @@ include get_theme_file_path( '/svg/ouroboros.svg' ); ?>
     </section>
 
     <section class="merch-gallery">
-      <img src="<?php echo esc_url( get_template_directory_uri() . '/images/dudekult-merch-vaatteet-1.jpg' ); ?>" alt="Valkoinen pellavapaita, jossa punainen Ouroboros-käärmekuvitus ja DUDE-teksti">
-      <img src="<?php echo esc_url( get_template_directory_uri() . '/images/dudekult-merch-vaatteet-2.jpg' ); ?>" alt="Musta huppari, jossa valkoinen Ouroboros-käärmekuvitus ja DUDE-teksti">
-      <img class="bigger" src="<?php echo esc_url( get_template_directory_uri() . '/images/dudekult-merch-vaatteet-5.jpg' ); ?>" alt="Musta huppari, jossa valkoinen Ouroboros-käärmekuvitus ja DUDE-teksti">
-      <img src="<?php echo esc_url( get_template_directory_uri() . '/images/dudekult-merch-vaatteet-3.jpg' ); ?>" alt="Nainen seisoo edustalla kainalossaan DUDE-kangaskassi, jossa musta Ouroboros-kuvitus ja DUDE-logo, taustalla sumeana kaksi miestä nojaamassa seinään">
-      <img src="<?php echo esc_url( get_template_directory_uri() . '/images/dudekult-merch-vaatteet-4.jpg' ); ?>" alt="Musta huppari, jossa valkoinen Ouroboros-käärmekuvitus ja DUDE-teksti">
+      <img src="<?php echo esc_url( get_template_directory_uri() . '/images/dudekult-merch-vaatteet-1-2.jpg' ); ?>" alt="Valkoinen pellavapaita, jossa punainen Ouroboros-käärmekuvitus ja DUDE-teksti">
+      <img src="<?php echo esc_url( get_template_directory_uri() . '/images/dudekult-merch-vaatteet-2-2.jpg' ); ?>" alt="Musta huppari, jossa valkoinen Ouroboros-käärmekuvitus ja DUDE-teksti">
+      <img class="bigger" src="<?php echo esc_url( get_template_directory_uri() . '/images/dudekult-merch-vaatteet-5-2.jpg' ); ?>" alt="Musta huppari, jossa valkoinen Ouroboros-käärmekuvitus ja DUDE-teksti">
+      <img src="<?php echo esc_url( get_template_directory_uri() . '/images/dudekult-merch-vaatteet-3-2.jpg' ); ?>" alt="Nainen seisoo edustalla kainalossaan DUDE-kangaskassi, jossa musta Ouroboros-kuvitus ja DUDE-logo, taustalla sumeana kaksi miestä nojaamassa seinään">
+      <img src="<?php echo esc_url( get_template_directory_uri() . '/images/dudekult-merch-vaatteet-4-2.jpg' ); ?>" alt="Musta huppari, jossa valkoinen Ouroboros-käärmekuvitus ja DUDE-teksti">
     </section>
 
 	</main><!-- #main -->
