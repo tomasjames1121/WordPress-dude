af2f6d544723363dd52595db01293c5de3c12e30
tomasjames1121 <tomasjames1121@gmail.com>
2021-10-12 12:35:42 +0300

    Job apply form improvements

--- content/themes/dude/template-parts/modules/form-job.php
+++ content/themes/dude/template-parts/modules/form-job.php
@@ -3,7 +3,7 @@
  * @Author:             Roni Laukkarinen
  * @Date:               2021-10-11 16:08:55
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2021-10-12 10:54:13
+ * @Last Modified time: 2021-10-12 12:34:34
  *
  * @package dude
  */
@@ -13,6 +13,10 @@
   <div class="container">
 
     <h2>Lähetä meille hakemus</h2>
+
+    <div class="description">
+      <p>Voit lähettää hakemuksesi myös suoraan sähköpostitse <a href="https://www.dude.fi/dudet/roni">Ronille</a> osoitteeseen <a href="mailto:roni@dude.fi">roni@dude.fi</a> tai soitella paikasta puhelimitse <a href="https://www.dude.fi/dudet/kristian">Kristianille</a> numeroon <a href="tel:0408351033">040 835 1033</a>.
+    </div>
 
     <?php gravity_form(
       $id = 11,
