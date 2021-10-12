fda53282d8eda574d46896ca45db4e9b7b90a3c1
tomasjames1121 <tomasjames1121@gmail.com>
2021-10-12 11:34:58 +0300

    Jobs form, general form fixes for gravity forms 2.5

--- content/themes/dude/template-parts/modules/form-job.php
+++ content/themes/dude/template-parts/modules/form-job.php
@@ -3,19 +3,19 @@
  * @Author:             Roni Laukkarinen
  * @Date:               2021-10-11 16:08:55
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2021-10-12 10:17:26
+ * @Last Modified time: 2021-10-12 10:54:13
  *
  * @package dude
  */
 ?>
 
-<section class="block form-job has-light-bg">
+<section class="block block-form-job has-light-bg">
   <div class="container">
 
     <h2>Lähetä meille hakemus</h2>
 
     <?php gravity_form(
-      $id = 10,
+      $id = 11,
       $display_title = false,
       $display_description = false,
       $display_inactive = false,
