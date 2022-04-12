5c908e3bbf2dd5148f6511f9d5b18f113e25c6e4
tomasjames1121 <tomasjames1121@gmail.com>
2022-04-12 11:37:13 +0300

    Add new client logos

--- content/themes/dude/template-parts/modules/logos.php
+++ content/themes/dude/template-parts/modules/logos.php
@@ -3,7 +3,7 @@
  * @Author:             Roni Laukkarinen, Digitoimisto Dude Oy (https://dude.fi)
  * @Date:               2020-07-15 18:34:29
  * @Last Modified by:   Roni Laukkarinen
- * @Last Modified time: 2021-10-12 14:57:19
+ * @Last Modified time: 2022-04-12 11:36:46
  *
  * @package dude
  */
@@ -27,11 +27,11 @@
       <div><?php include get_theme_file_path( '/svg/logos/berner.svg' ); ?></div>
       <div><?php include get_theme_file_path( '/svg/logos/northpatrol.svg' ); ?></div>
       <div><?php include get_theme_file_path( '/svg/logos/hotellialba.svg' ); ?></div>
-      <div class="logo-mediashake"><?php include get_theme_file_path( '/svg/logos/mediashake.svg' ); ?></div>
-      <div><?php include get_theme_file_path( '/svg/logos/crmservice.svg' ); ?></div>
+      <div class="logo-mediashake"><?php include get_theme_file_path( '/svg/logos/workpower.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/otava.svg' ); ?></div>
       <div class="logo-kktavastia"><?php include get_theme_file_path( '/svg/logos/kktavastia.svg' ); ?></div>
       <div><?php include get_theme_file_path( '/svg/logos/liiga.svg' ); ?></div>
-      <div><?php include get_theme_file_path( '/svg/logos/teeleidi.svg' ); ?></div>
+      <div><?php include get_theme_file_path( '/svg/logos/sofioksanen.svg' ); ?></div>
       <div class="logo-blackbruin"><?php include get_theme_file_path( '/svg/logos/blackbruin.svg' ); ?></div>
       <div class="logo-safa"><?php include get_theme_file_path( '/svg/logos/safa.svg' ); ?></div>
       <div class="logo-wotkins"><?php include get_theme_file_path( '/svg/logos/wotkins.svg' ); ?></div>
